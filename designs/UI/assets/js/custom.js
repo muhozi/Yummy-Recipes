9e3c7612e2910eeee7a6480b7f3498c27e0176d1
Muhozi Emery <muhozie@gmail.com>
2017-10-16 00:04:40 +0200

    Add Registration page(index.html) Login page, Recipes pages Scaffolding(recipes.html,addrecipe.html) and Login and Registration Wireframe

---
+++ designs/UI/assets/js/custom.js
@@ -0,0 +1,12 @@
+$('.gr-input').focus(function(){
+	$(this).prev('.input-group-btn').addClass('input-group-btn-active');
+});
+$('.gr-input').blur(function(){
+	$(this).prev('.input-group-btn').removeClass('input-group-btn-active');
+});
+$('.gr-textarea').focus(function(){
+	$(this).prev('.input-group-btn').addClass('input-group-btn-active');
+});
+$('.gr-textarea').blur(function(){
+	$(this).prev('.input-group-btn').removeClass('input-group-btn-active');
+});
