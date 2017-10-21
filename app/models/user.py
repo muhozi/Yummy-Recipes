bf61b4cf392f108da797d6e515597fc56e445234
Muhozi Emery <muhozie@gmail.com>
2017-10-21 08:06:01 +0200

    Add user Registration and user Authentication

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,19 @@
 from app.models.store import Store
+from werkzeug.security import check_password_hash
 import uuid
 class User(object):
 	"""User Model"""
-	def __init__(self, name, email, password, created, user_id=None,):
+	name = None
+	email = None
+	password = None
+	user_id = None
+	created = None
+	def save(self, name, email, password, created, user_id=None):
 		self.name = name
 		self.email = email
 		self.password = password
 		self.user_id = uuid.uuid4().hex if user_id is None else user_id
 		self.created = created
-	def save(self):
 		data = {
 			'id': self.user_id,
 			'name': self.name,
@@ -16,6 +21,26 @@ class User(object):
 			'password': self.password,
 			'created_at': self.created,
 		}
+		if (self.email_exist(self.email)):
+			return False
 		Store().add_user(data)
 		return True
+	def email_exist(self,email):
+		users = Store().get_users()
+		for user in users:
+			if (user['email'] == email):
+				return True
+		return False
+	def exist(self,email,password):
+		users = Store().get_users()
+		print(users)
+		for user in users:
+			# print(check_password_hash(user['password'], password))
+			if (user['email'] == email and check_password_hash(user['password'], password)):
+				self.user_id = user['id']
+				self.name = user['name']
+				self.email = user['email']
+				self.password = user['password']
+				return True
+		return False
 		
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,session,redirect,flash,request,url_for,abort
+from functools import wraps
 from werkzeug.security import generate_password_hash
 from datetime import datetime
 from app import app
@@ -6,7 +7,30 @@ from app.forms import RegisterForm,LoginForm,RecipeForm,CategoryForm
 from app.models.store import Store;
 from app.models.user import User;
 store = Store();
+""" Only logged in user Middleware function"""
+def auth(n):
+	@wraps(n)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session and session['logged_in'] == True:
+			return n(*args, **kwargs)
+		else:
+			return redirect(url_for('login'))
+	return wrap
+
+""" Only Guest user Middleware function"""
+def guest(n):
+	@wraps(n)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return redirect(url_for('recipes'))
+		else:
+			return n(*args, **kwargs)
+
+	return wrap
+
+"""Registration route | Index route"""
 @app.route('/',methods=['POST','GET'])
+@guest
 def join():
 	error=None
 	print(store.get_users())
@@ -15,31 +39,67 @@ def join():
 		return render_template("index.html",form = form)
 	elif (request.method == 'POST'):
 		if form.validate_on_submit():
-			user = User(
+			user = User()
+			user_saved = user.save(
 				name=request.form['name'],
 				email=request.form['email'],
 				password=generate_password_hash(request.form['password']),
 				created=datetime.now()
 			)
-			user.save()
-			# User successfully registered
-			return redirect(url_for('recipes'))
+			if ( user_saved ):
+				session['logged_in'] = True
+				session['id'] = user.user_id
+				session['name'] = user.name
+				session['email'] = user.email
+				flash('welcome to Yummy recipes community',category='successMessage')
+				return redirect(url_for('recipes'))
+			flash('Email already exists',category='errorMessage')
+			flash(request.form,category='input')
+			return redirect(redirect_back())
 		error = form.errors
 		flash(error,category='error')
 		flash(request.form,category='input')
 		return redirect(redirect_back())
 	return abort(404)
 
+"""Login route"""
 @app.route('/login',methods = ['POST','GET'])
+@guest
 def login():
+	error = None
+	form = LoginForm.Login()
 	if (request.method == 'GET'):
-		form = LoginForm.Login()
 		return render_template("login.html",form = form)
 	elif (request.method == 'POST'):
-		return "POST"
-	return "OK"
+		if form.validate_on_submit():
+			user = User()
+			user_exist = user.exist(request.form['email'],request.form['password'])
+			if ( user_exist ):
+				session['logged_in'] = True
+				session['id'] = user.user_id
+				session['name'] = user.name
+				session['email'] = user.email
+				flash('welcome back to Yummy recipes community',category='successMessage')
+				return redirect(url_for('recipes'))
+			flash('Invalid email or password',category='errorMessage')
+			flash(request.form,category='input')
+			return redirect(redirect_back())
+		error = form.errors
+		flash(error,category='error')
+		flash(request.form,category='input')
+		return redirect(redirect_back())
+	return abort(404)
 
+"""Logout route"""
+@app.route('/logout')
+@auth
+def logout():
+	session.clear()
+	return redirect(url_for('login'))
+
+"""Add Recipe route"""
 @app.route('/addrecipe')
+@auth
 def addrecipe():
 	users = store.get_users();
 	print(users);
@@ -50,7 +110,16 @@ def redirect_back(default='index'):
 	return request.args.get('next') or \
 		request.referrer or \
 		url_for(default)
+
+""" Redirect back """
+def redirect_back(default='index'):
+	return request.args.get('next') or \
+		request.referrer or \
+		url_for(default)
+
+"""Recipes route"""
 @app.route('/recipes')
+@auth
 def recipes():
 	#Edit Recipe form details
 	form = RecipeForm.EditForm()
