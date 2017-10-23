9263b3b3c00424e740a5a7f0a52dd5b7ed6d4fe0
Muhozi Emery <muhozie@gmail.com>
2017-10-23 17:06:22 +0200

    Change config
    change SERVER_NAME config

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # CSRF Protection
 CSRF_ENABLED = True
-SERVER_NAME = "localhost:5000"
 
 CSRF_SESSION_KEY = "hBtiRRnXTEkOW347jy5py/qKn29oVb95qEeoYXv337I"
 # Secret key | for cookies
